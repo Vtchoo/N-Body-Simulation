0aa9bf59e5c7e8d106fed6605563afedd87a912f
Vtchoo <vittor-limma@hotmail.com>
2020-09-13 19:19:44 -0300

    First files

---
+++ simulation.js
@@ -0,0 +1,21 @@
+// Constants
+const canvasWidth = window.innerWidth
+const canvasHeight = window.innerHeight
+
+
+
+// Simulation setup
+function setup(){
+
+
+
+
+
+    createCanvas(canvasWidth, canvasHeight)
+}
+
+// Update function
+function draw() {
+    background(0)
+}
+
